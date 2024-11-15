ee6a56201604e1bd278814a2715eb8693d702ed1
Ekta Khandelwal <ektakhandelwal@Ektas-MacBook-Pro.local>
2024-11-14 19:48:58 -0800

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>Modified By Ekta
           count is {count}
         </button>
         <p>
